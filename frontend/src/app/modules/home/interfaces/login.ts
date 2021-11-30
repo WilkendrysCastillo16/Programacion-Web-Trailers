6f36525878e54f5bf3d370deee5744068328f63a
elianmtg <elianmtoribio@gmail.com>
2021-11-29 22:36:31 -0400

    interfaces bases

---
+++ frontend/src/app/modules/home/interfaces/login.ts
@@ -0,0 +1,5 @@
+export interface ILogin {
+    email: string,
+    pass: string
+}
+
---
+++ frontend/src/app/modules/home/interfaces/trailer.ts
@@ -0,0 +1,10 @@
+export interface ITrailer {
+    id: number,
+    title: string,
+    director: string,
+    review: string,
+    yearTrailer: Date,
+    cover: string,
+    link: string,
+    rating: number
+}
