42e2454148b1475449558209d6f1347317eabace
elianmtg <elianmtoribio@gmail.com>
2021-12-02 01:52:12 -0400

    cerrar detalles

--- frontend/src/app/modules/home/components/detalles/detalles.component.ts
+++ frontend/src/app/modules/home/components/detalles/detalles.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ITraillerActor } from '../../interfaces/trailler-actor';
 import { IActor } from './../../interfaces/actor';
+import { PrincipalComponent } from '../../pages/principal/principal.component';
 
 @Component({
   selector: 'app-detalles',
@@ -13,9 +14,14 @@ export class DetallesComponent implements OnInit {
   trailer!: ITraillerActor;
   actor!: IActor;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public detalle: {trailer: ITraillerActor}) { }
+  constructor(public dialogRef: MatDialogRef<PrincipalComponent>,@Inject(MAT_DIALOG_DATA) public detalle: {trailer: ITraillerActor}) { }
 
   ngOnInit(): void {
+    console.log(this.detalle);
+  }
+
+  onHome(): void {
+    this.dialogRef.close();
   }
 
 }
