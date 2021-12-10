0042e66bba7f50b92483d85ec4e2ee047439556c
KeuryLendof <lendofkeury@gmail.com>
2021-12-10 14:38:12 -0400

    todo bien

--- frontend/src/app/core/header/header.component.ts
+++ frontend/src/app/core/header/header.component.ts
@@ -11,7 +11,7 @@ export class HeaderComponent implements OnInit {
   emailAdmin = localStorage.getItem("Email"); 
   constructor(private router:Router) { 
   }
-
+ 
   ngOnInit(): void {
     const toggleButton = document.getElementsByClassName('toggle-button')[0]
     const navbarActive = document.getElementsByClassName('navbar-links')[0]
